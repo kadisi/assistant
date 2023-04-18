309ca4baae059168ccd0f396c7bcdb15c25fc067
bingyu <bingyu.zj@alibaba-inc.com>
2023-04-18 02:26:13 -0400

    change version

--- get-version.sh
+++ get-version.sh
@@ -1,19 +1,20 @@
 # Return the current git tag (if present) or the first characters of the commit hash
 #!/usr/bin/env sh 
-
+#set -x
 GIT_HASH=$(git log -n1 --pretty='%h')
-DESCRIBE=$(git describe --exact-match --tags $GIT_HASH 2> /dev/null)
+#DESCRIBE=$(git describe --exact-match --tags $GIT_HASH 2> /dev/null)
 
-if [ -n $DESCRIBE ]; then
+#if [ -n $DESCRIBE ]; then
     # Remove 'v' from beginning of tag, if present before a number
-    RESULT=$(echo $DESCRIBE | sed 's/^v\([0-9]\)/\1/')
-else
-    RESULT=$GIT_HASH
-fi
+#    RESULT=$(echo $DESCRIBE | sed 's/^v\([0-9]\)/\1/')
+#else
+#    RESULT=$GIT_HASH
+#fi
 
 # Add a * to the hash if there are uncommitted changes
-if [ -n "$(git status -s)" ]; then
-    RESULT+="*"
-fi
+#if [ -n "$(git status -s)" ]; then
+#    RESULT+="*"
+#fi
 
+RESULT=$GIT_HASH
 echo $RESULT
